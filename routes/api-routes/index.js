0c147e09ce4d18698def04fe3afef1bcf7d8b69e
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-02 14:36:53 -0500

    adds get route to api router

--- routes/api-routes/index.js
+++ routes/api-routes/index.js
@@ -2,3 +2,10 @@ const fs = require("fs");
 const db = require("../../db/db.json");
 const router = require("express").Router();
 const uniqid = require("uniqid");
+
+router.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) throw err;
+    res.send(data);
+  });
+});
