a6f85a492173c5924ff81345ef7ddb72cb562e4a
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-02 14:39:30 -0500

    adds initial post route to api router

--- routes/api-routes/index.js
+++ routes/api-routes/index.js
@@ -1,11 +1,26 @@
+// DEPENDENCIES
 const fs = require("fs");
 const db = require("../../db/db.json");
 const router = require("express").Router();
 const uniqid = require("uniqid");
 
+// get route for api router
 router.get("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", (err, data) => {
     if (err) throw err;
     res.send(data);
+  });
+});
+
+// post route for api router
+router.post("/api/notes", (req, res) => {
+  let newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uniqid(),
+  };
+
+  fs.readFile("./db/db.json", (req, res) => {
+    if (err) throw err;
   });
 });
