267d41d3b2bc61c663a73746f7e04616713a81ed
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-02 14:43:56 -0500

    adds fs read and write file to post route, adds comments

--- routes/api-routes/index.js
+++ routes/api-routes/index.js
@@ -6,6 +6,7 @@ const uniqid = require("uniqid");
 
 // get route for api router
 router.get("/api/notes", (req, res) => {
+  // use fs read file to send the data
   fs.readFile("./db/db.json", (err, data) => {
     if (err) throw err;
     res.send(data);
@@ -14,13 +15,25 @@ router.get("/api/notes", (req, res) => {
 
 // post route for api router
 router.post("/api/notes", (req, res) => {
+  // variable to hold new note data
   let newNote = {
     title: req.body.title,
     text: req.body.text,
+    // unique id function
     id: uniqid(),
   };
 
+  // use fs readFile to push the new data
   fs.readFile("./db/db.json", (req, res) => {
     if (err) throw err;
+
+    let newData = JSON.parse(data);
+    newData.push(newNote);
+
+    // fs write file to update db with new data
+    fs.writeFile("./db/db.json", JSON.stringify(newData), (err) => {
+      if (err) throw err;
+      res.send("New Data Added Successfully!");
+    });
   });
 });
